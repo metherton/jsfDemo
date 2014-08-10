26a5f393f16e52e63962885dea11f9a5149e7793
metherton <martinetherton63@googlemail.com>
2014-08-10 21:42:14 +0200

    added new styles

--- target/jsfDemo-1.0/WEB-INF/classes/META-INF/db/schema.sql
+++
@@ -1,6 +0,0 @@
-drop table T_USER if exists;
-drop table T_PARTNER if exists;
-
-
-create table T_USER (ID integer identity primary key, BSN varchar(9), SURNAME varchar(50),  FIRST_NAME varchar(50) not null, DATE_OF_BIRTH date, PARTNER_ID integer);
-create table T_PARTNER (ID integer identity primary key, BSN varchar(9));
